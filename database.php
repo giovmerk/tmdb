6e7d0672cd954c572f2c2df859c287679788164c
giovmerk <giovmerk@gmail.com>
2024-06-06 17:53:27 +0200

    create file database

---
+++ database.php
@@ -0,0 +1,82 @@
+<?php
+#dichiaro la classe database e dichiaro delle funzioni all'interno
+class Database
+{
+    private $host = '127.0.0.1';
+    private $db_name = 'TMDB';
+    private $username = 'root';
+    private $password = 'root';
+    private $port = 3306;
+    public $conn;
+
+    public function __construct()
+    {
+		$conn = null;
+		try
+		{
+			$this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
+		}
+		catch(PDOException $exception)
+		{
+			echo "Errore di connessione: " . $exception->getMessage();
+		}
+
+	}
+
+    public function read($param, $table, $condition) #manda a schermo (utilizzabile come select)
+    {
+        $query = "SELECT $param FROM $table";
+        if ($condition!==null)
+        {
+            $query = $query . " WHERE $condition";
+            var_dump($query);
+        }
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        #var_export manda a schermo in maniera semplificata, fetchAll trasforma l'output in json, PDO::FETCH_ASSOC associa i valori e li rende univoci
+        var_export($stmt->fetchAll(PDO::FETCH_ASSOC));
+        return $stmt;
+    }
+
+
+    public function create($table, $param, $data) #inserisce dei datti all'interno di un database (utilizzabile come insert)
+    {
+        $string = implode(', ', $data);
+        // $string = mysql_escape_string($string);
+        $query = "INSERT IGNORE INTO $table($param) VALUES ($string)";
+        echo $query;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        // var_export($stmt->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    public function update($table, $data, $condition) #aggiorna dei datti all'interno di un database (utilizzabile come update)
+    {
+        $query = `UPDATE $table SET $data`;
+        if ($condition!==null)
+        {
+            $query = $query . `WHERE $condition`;
+        }
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        var_export($stmt->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    public function delete($param, $table, $condition) #cancewlla dei datti all'interno di un database (utilizzabile come delete)
+    {
+        $query = `DELETE $param FROM $table`;
+        if ($condition!==null)
+        {
+            $query = $query . `WHERE $condition`;
+        }
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        var_export($stmt->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+   
+    public function real_escape_string($string) {
+        return $this->conn->quote($string);
+    }
+
+}
